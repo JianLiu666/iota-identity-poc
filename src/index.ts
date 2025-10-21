ba5046f3e82ec73425ec238f61ffe055c28f4f51
jian.liu <jianliu0616@gmail.com>
2025-10-21 23:51:57 +0800

    feat: create did flow

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-const message: string = "Hello World";
+import { IotaClient } from '@iota/iota-sdk/client';
+import { createDocumentForNetwork, getFundedClient, getMemStorage } from './util';
+import { IotaDID } from '@iota/identity-wasm/node';
 
-console.log(message);
+async function createIdentity(): Promise<void> {
+  // create new client to connect to IOTA network
+  const iotaClient = new IotaClient({ url: 'https://api.testnet.iota.cafe' });
+  const network = await iotaClient.getChainIdentifier();
+
+  // create new client that offers identity related functions
+  const storage = getMemStorage();
+  const identityClient = await getFundedClient(storage);
+
+  // create new unpublished document
+  const [unpublished] = await createDocumentForNetwork(storage, network);
+  console.log(`Unpublished DID document: ${JSON.stringify(unpublished, null, 2)}`);
+
+  let did: IotaDID;
+  console.log('Creating new identity');
+  const { output: identity } = await identityClient
+    .createIdentity(unpublished)
+    .finish()
+    .buildAndExecute(identityClient);
+  did = identity.didDocument().id();
+
+  // check if we can resolve it via client
+  const resolved = await identityClient.resolveDid(did);
+  console.log(`Resolved DID document: ${JSON.stringify(resolved, null, 2)}`);
+}
+
+createIdentity().catch((error) => {
+  console.error('Example error:', error);
+});
---
+++ src/util.ts
@@ -0,0 +1,75 @@
+import {
+  IdentityClient,
+  IdentityClientReadOnly,
+  IotaDocument,
+  JwkMemStore,
+  JwsAlgorithm,
+  KeyIdMemStore,
+  MethodScope,
+  Storage,
+  StorageSigner,
+} from '@iota/identity-wasm/node';
+import { IotaClient, Network } from '@iota/iota-sdk/client';
+import { getFaucetHost, requestIotaFromFaucetV0 } from '@iota/iota-sdk/faucet';
+
+export function getMemStorage(): Storage {
+  return new Storage(new JwkMemStore(), new KeyIdMemStore());
+}
+
+export async function createDocumentForNetwork(
+  storage: Storage,
+  network: string,
+): Promise<[IotaDocument, string]> {
+  // Create a new DID document with a placeholder DID.
+  const unpublished = new IotaDocument(network);
+  const verificationMethodFragment = await unpublished.generateMethod(
+    storage,
+    JwkMemStore.ed25519KeyType(),
+    JwsAlgorithm.EdDSA,
+    '#key-1',
+    MethodScope.VerificationMethod(),
+  );
+
+  return [unpublished, verificationMethodFragment];
+}
+
+export async function requestFunds(address: string) {
+  await requestIotaFromFaucetV0({
+    host: getFaucetHost(Network.Testnet),
+    recipient: address,
+  });
+}
+
+export async function getFundedClient(storage: Storage): Promise<IdentityClient> {
+  const iotaClient = new IotaClient({
+    url: 'https://api.testnet.iota.cafe',
+  });
+
+  const identityClientReadOnly = await IdentityClientReadOnly.create(iotaClient as unknown as any);
+
+  // generate new key
+  let generate = await storage.keyStorage().generate('Ed25519', JwsAlgorithm.EdDSA);
+
+  let publicKeyJwk = generate.jwk().toPublic();
+  if (typeof publicKeyJwk === 'undefined') {
+    throw new Error('failed to derive public key JWK from generated JWK');
+  }
+  let keyId = generate.keyId();
+
+  // create signer from storage
+  let signer = new StorageSigner(storage, keyId, publicKeyJwk);
+  const identityClient = await IdentityClient.create(identityClientReadOnly, signer);
+
+  await requestFunds(identityClient.senderAddress());
+
+  const balance = await iotaClient.getBalance({ owner: identityClient.senderAddress() });
+  if (balance.totalBalance === '0') {
+    throw new Error('Balance is still 0');
+  } else {
+    console.log(
+      `Received gas from faucet: ${balance.totalBalance} for owner ${identityClient.senderAddress()}`,
+    );
+  }
+
+  return identityClient;
+}
