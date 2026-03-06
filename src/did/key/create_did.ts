7800249919305f2bd5dd56543e92b8b0bab72b40
jian.liu <jianliu0616@gmail.com>
2026-03-06 18:12:31 +0800

    feat: update func

--- src/did/key/create_did.ts
+++ src/did/key/create_did.ts
@@ -3,10 +3,10 @@ import bs58 from 'bs58';
 const X_BASE64URL = '...';
 const Y_BASE64URL = '...';
 
-function createDidKey() {
+function createDidKey(xBase64url: string, yBase64url: string) {
   // 轉回 Buffer
-  const x = Buffer.from(X_BASE64URL, 'base64url');
-  const y = Buffer.from(Y_BASE64URL, 'base64url');
+  const x = Buffer.from(xBase64url, 'base64url');
+  const y = Buffer.from(yBase64url, 'base64url');
 
   // 根據 Y 的最後一個 byte 判斷是奇數還是偶數來決定 prefix (0x02 偶數, 0x03 奇數)
   const prefix = y[y.length - 1] % 2 === 0 ? 0x02 : 0x03;
@@ -23,4 +23,4 @@ function createDidKey() {
   console.log(correctDidKey);
 }
 
-createDidKey();
+createDidKey(X_BASE64URL, Y_BASE64URL);
